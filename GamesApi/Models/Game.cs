7e564b50b138e4d79545583ab0ae64587cf3c134
dazzes4545 <dazzes7619@gmail.com>
2026-04-17 09:56:24 +0300

    Step 5: GamesController with GET, POST, DELETE, PUT

---
+++ GamesApi/Models/Game.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Net.Http.Headers;
+
+namespace GameApi.Models;
+public class Game {
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Genre { get; set; } = string.Empty;
+    public int ReleaseYear { get; set; }
+}
